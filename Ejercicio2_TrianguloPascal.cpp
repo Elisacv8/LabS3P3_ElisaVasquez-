272eb4daca2bad72e4697c776b6019dac4defffb
Elisacv8 <140852045+Elisacv8@users.noreply.github.com>
2023-08-04 15:39:18 -0600

    Update Ejercicio2_TrianguloPascal.cpp

--- Ejercicio2_TrianguloPascal.cpp
+++ Ejercicio2_TrianguloPascal.cpp
@@ -5,6 +5,7 @@
 #include <vector>
 using namespace std;
 
+//Menu
 void mostrarMenu() {
     cout << "======== TRIÁNGULO DE PASCAL =======" << endl;
     cout << "--------------- MENU ---------------" << endl;
@@ -12,6 +13,8 @@ void mostrarMenu() {
     cout << "2. Salir" << endl;
 }
 
+// Esta funcion recibe como parametro un vector de enteros y
+// Se encarga de imprimir los elementos del vector en consola
 void imprimirArray(const vector<int>& arr) {
     for (int num : arr) {
         cout << num << " ";
@@ -19,6 +22,7 @@ void imprimirArray(const vector<int>& arr) {
     cout << endl;
 }
 
+//Esta es una funcion recursiva el cual genera una linea del triangulo de pascal
 vector<int> obtenerLineaPascal(int n) {
     if (n == 1) {
         return { 1 };
@@ -31,4 +35,45 @@ vector<int> obtenerLineaPascal(int n) {
         }
         return current;
     }
+}
+
+// Funcion principal del programa
+// En esta funcion use do-while el cual permite que se repita el menu hasta el user 
+// decida salirse.
+int main() {
+    char opcion;
+    do {
+        mostrarMenu();
+        cout << "¿Qué opción desea elegir? ";
+        cin >> opcion;
+
+        switch (opcion) {
+        case '1': {
+            int numIteraciones;
+            cout << "Ingrese el número de iteraciones (1-50): ";
+            cin >> numIteraciones;
+
+            if (numIteraciones < 1 || numIteraciones > 50) {
+                cout << "El número de iteraciones debe estar entre 1 y 50." << endl;
+            }
+            else {
+                for (int i = 1; i <= numIteraciones; i++) {
+                    vector<int> lineaPascal = obtenerLineaPascal(i);
+                    cout << "Iteración " << i << ": ";
+                    imprimirArray(lineaPascal);
+                }
+            }
+            break;
+        }
+        case '2':
+            cout << "Saliendo del programa..." << endl;
+            break;
+        default:
+            cout << "Opción inválida. Por favor, ingrese una opción válida." << endl;
+            break;
+        }
+
+    } while (opcion != '2');
+
+    return 0;
 }
